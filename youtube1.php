2f8eeab20f2636108b58c31659dd2e20c1f9c9ff
meriduniya-pixel <70277620+meriduniya-pixel@users.noreply.github.com>
2020-08-26 21:34:08 +0530

    Add files via upload

---
+++ youtube1.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="Content-Language" content="en" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+       <!-- Main style -->
+        <link href="theme.css" rel="stylesheet" />
+        
+        <!-- Font-Awesome -->
+        <link href="font-awesome.min.css" rel="stylesheet" />
+                
+        <!-- Custom Theme style -->
+    
+
+ <link href="custom.css" rel="stylesheet" />
+
+
+
+
+  
+        
+                
+        <!-- jQuery 1.10.2 -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        
+            </head>
+
+<body>   
+   <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse-menu">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="">
+                      <img class="themeLogoImg" src="logo.jpg" />                    </a>
+                   </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="collapse-menu">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="">Home</a></li><li><a href="">Contact US</a></li>
+            					<ul class="dropdown-menu">
+                          		</li>
+                                               </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container -->
+    </nav><!--/.navbar-->  
+
+</header>
+<center>
+
+
+<center>
+
+
+  <h1>Youtube Video Downloader Tool</h1>
+  <br>
+  <fieldset>
+<form method="POST" action="youtube2.php" enctype="multipart/form-data">
+  <br>
+        <input type="text" name="youtubeURL" Placeholder="Enter Youtube Video URL" size="70"><br>
+        <br>
+        <input type="submit" value="Download"  name="submit">
+
+      </form>
+
+</fieldset>
+    </center>
+</center>
+
+  </div>
+</div>
+  
+</div>
+</div>   
+<h2>Youtube Video Downloader</h2><br>
+<b>
+Alexa site rank furnished by searchenginereports.com will arrive handy if want to examine the standing of your website on a regular basis. All You should do is to write down the website URL in Area provided and click on over the “Examine” button.
+
+This Resource generates a traffic record graph for any chosen website page. It computes the website traffic of a particular web site by evaluating the world wide web utilization of Alexa Toolbars people, which happen to be anticipated to generally be millions of folks. With the knowledge it provides, you are able to Examine if the Website positioning procedures you've utilised to spice up your site’s popularity are successful or not.
+
+Alexa ranking is a straightforward ranking procedure for almost any Site which happens to be an presenting of alexa.com. By using a straightforward algorithm, alexa.com measures the frequency of views on a web site. The targeted traffic is calculated dependant on parameters like reach and webpage views of any webpage.
+
+A relative standard of viewers overlap between this site and related sites. Viewers overlap rating is calculated from an analysis of prevalent visitors and/or research search phrases.
+
+Alexa has made over time to include a paid out selection that features a suite of tools for examining Search engine optimization, conducting key phrase audits, and examining your competitor's effectiveness.
+
+To totally utilize this, the authority with the website page is ranked concerning the figures one-a hundred. The implication of this is that the page will only get a higher ranking if it's the chance to be ranked significant.
+
+Examine how well your web page is optimized to the concentrate on key phrase. We’ll operate a report and offer you actionable ways to increase your probability of ranking.
+
+Alexa can be a subsidiary business of Amazon.com that was acquired during the 12 months 1999. This is a California dependent organization that specializes in delivering web website traffic information and facts collected from different resources like World wide web browser extensions and toolbars.
+
+A relative degree of viewers overlap between This page and related web-sites. Audience overlap score is calculated from an Assessment of common guests and/or lookup key terms.
+
+And, Alivenet Remedy thinks that it's important for every business enterprise and entrepreneurs to evaluation their Site rankings periodically and make sure it progresses in the direction of the bottom quantity. The Alexa Visitors Rank Checker is quick and simple to use. It helps in keeping track of crucial parameters to raise site visitors and income and generate a lot more revenue.
+
+And the website Using the least variety is going to be ranked around 40 million. Additionally, if Alexa’s Site rank checker can not get to a specific Internet site for over the past a few months, there'll be no rank at all for the website.
+
+Smart shows Take care of your calendar, abide by as well as recipes, check here catch up on news plus more with Alexa.
+
+Many of our prospects are very well-known organizations. They opt for us thanks to our know-how and discretion. Should they trust us to boost their Site rank, why shouldn’t you?
+
+Users usually get drawn to one thing distinctive and so they finally become the automatic promoters for the web site which only Advantages it. So it only ensures that-
+
+
+</b>
+
+  	</div>
+</div> <br /> <br />
+
+
+<?php include'footer.php'; ?>  
+  
+	
+</body>
+</html>
+
---
+++ yt.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Refresh the Page with JavaScript</title>
+<script>
+    function reloadPage(){
+        location.reload(true);
+    }
+</script>
+</head>
+<body>
+    <button type="button" onclick="reloadPage();">Reload page</button>   
+</body> 
+</html>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="Content-Language" content="en" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+       <!-- Main style -->
+        <link href="theme.css" rel="stylesheet" />
+        
+        <!-- Font-Awesome -->
+        <link href="font-awesome.min.css" rel="stylesheet" />
+                
+        <!-- Custom Theme style -->
+    
+
+ <link href="custom.css" rel="stylesheet" />
+
+
+
+
+  
+        
+                
+        <!-- jQuery 1.10.2 -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        
+            </head>
+
+<body>   
+   <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse-menu">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+              
+                         
+                   </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="collapse-menu">
+                    <ul class="nav navbar-nav navbar-right">
+                  			<ul class="dropdown-menu">
+                          		</li>
+                                               </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container -->
+    </nav><!--/.navbar-->  
+
+</header>
+<center>
+<!DOCTYPE html>
+<html dir='ltr' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+<head>
+<title>Youtube Thumbnail Downloader</title>
+<meta content='YouTube Thumbnail Downloader automatically Generate High Quality Thumbnails or Images from YouTube videos. Paste your YouTube Video URL in the below box and Click on "Download" button. Within Second it will generate your sweet thumbnails.' name='Description'/>
+<meta content='YouTube Thumbnail Downloader,youtube, thumbnail, generator, screenshots, image generator, picture generator, maker, photo, creater,' name='Keywords'/>
+<meta content='index, follow' name='robots'/>
+<meta content='2 days' name='revisit-after'/>
+<meta content='English' name='language'/>
+<meta content='INDIA' name='geo.country'/>
+<meta content='MUMBAI' name='geo.placename'/>
+<meta content='global' name='distribution'/>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+<meta content='true' name='MSSmartTagsPreventParsing'/>
+<meta content='general' name='rating'/>
+<link href='https://websiteurl.com/' rel='canonical'/>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<style id='page-skin-1' type='text/css'><!--
+/*
+-----------------------------------------------------------------------------------
+Template Disign By Technical Arp Visit: www.Technicalarp.com
+----------------------------------------------------------------------------------- */
+#navbar-iframe {
+height:0px;
+visibility:hidden;
+display: none !important;
+}
+html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+margin: 0;
+padding: 0;
+border: 0;
+font-size: 100%;
+font: inherit;
+vertical-align: baseline;
+}
+article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
+display: block;
+}
+html {
+background:#eeeeee;
+text-align:center;
+line-height: 1;
+font-family: Georgia;
+}
+ol, ul {
+list-style: none;
+}
+blockquote, q {
+quotes: none;
+}
+blockquote:before, blockquote:after, q:before, q:after {
+content: '';
+content: none;
+}
+table {
+border-collapse: collapse;
+border-spacing: 0;
+}
+*:focus {
+outline: 0;
+}
+#hd {
+width:100%;
+height:390px;
+background:#C8312B;
+margin:0 0 50px 0;
+-moz-box-shadow: inset 0 -4px 10px -5px #000;
+-webkit-box-shadow: inset 0 -4px 10px -5px #000;
+box-shadow: inset 0 -4px 10px -5px #000;
+}
+h1 {
+font-size:24px;
+font-weight:bold;
+color:#fff;
+text-shadow: 1px 1px 1px #666666;
+}
+#hd p{
+max-width:70%;
+margin:20px auto;
+padding:10px;
+background:#eeeeee;
+border: dashed #600B0B 2px;
+font-family:arial;
+font-size:17px;
+font-weight:bold;
+line-height: 1.4;
+color:#600B0B;
+}
+#hd p span{
+color:#F05C5C;
+}
+input[type="text"]{
+width:400px;
+height:70px;
+padding:0 0 0 10px;
+border: dashed #1a1a1a 2px;
+font-family: Georgia;
+}
+input[type="submit"]{
+height:72px;
+background: #66bdff;
+background-image: -webkit-linear-gradient(top, #66bdff, #0d9eff);
+background-image: -moz-linear-gradient(top, #66bdff, #0d9eff);
+background-image: -ms-linear-gradient(top, #66bdff, #0d9eff);
+background-image: -o-linear-gradient(top, #66bdff, #0d9eff);
+background-image: linear-gradient(to bottom, #66bdff, #0d9eff);
+border: dashed #1a1a1a 1px;
+-webkit-border-radius: 0;
+-moz-border-radius: 0;
+border-radius: 0px;
+margin:0 0 0 -2px;
+cursor: pointer;
+font-family: Georgia;
+text-decoration: none;
+color: #fff;
+}
+input[type="submit"]:hover {
+background: #f72525;
+background-image: -webkit-linear-gradient(top, #f72525, #b52222);
+background-image: -moz-linear-gradient(top, #f72525, #b52222);
+background-image: -ms-linear-gradient(top, #f72525, #b52222);
+background-image: -o-linear-gradient(top, #f72525, #b52222);
+background-image: linear-gradient(to bottom, #f72525, #b52222);
+}
+h2{
+margin:20px;
+font-size:14px;
+}
+h3{
+color:#A9A6A6;
+margin:10px auto;
+}
+#hidden_div img{
+margin:5px;
+border: solid #C8312B 3px;
+}
+#sorry{
+width:200px;
+height:3opx;
+margin:10px auto;
+padding:10px;
+background:#BA2020;
+border: dashed #7E1313 2px;
+color:#fff;
+}
+#iam{
+margin:50px auto;
+color:#1a1a1a;
+}
+
+-->
+.adspacebytechnicalarp{padding:2em;box-shadow:0 5px 15px rgba(0,0,0,.16);border-radius:5px;margin-top:1em;background:#fff;text-align:left}
+</style>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js' type='text/javascript'></script>
+
+</head>
+<body class=" text-center">
+
+<h1>Youtube Thumbnail Downloader <script>document.write(new Date().getFullYear())</script></h1>
+
+<div class="addthis_inline_share_toolbox centersocials"></div>
+
+ 
+   </br>
+<div class="container">
+<form onsubmit='showHide(); return false;'>
+  <input autocomplete='on' id='technicalarpvid' placeholder='Paste A Youtube URL' type='text'/>
+  <input id='technicalarpvideoButton' onclick='showDiv()' type='submit' value='Download'/>
+</form>
+</div>
+
+ 
+   
+<div id='hidden_div' style='display:none'>
+<div id='thumbnails'>
+</div>
+</div>
+
+ 
+   
+
+
+
+
+<script Language='Javascript'>
+
+    $(document).ready(function() {
+      $('#technicalarpvideoButton').click(function() {
+      function GetQueryParam(parameter, address){
+      var p = escape(unescape(parameter));
+      var regex = new RegExp("[?&]" + p + "(?:=([^&]*))?","i");
+      var match = regex.exec(address);
+      var value = null;
+      if( match != null ){
+      value = match[1];
+      }
+      return value;
+      }
+      var videoURL = $('#technicalarpvid').val();
+      var theID = GetQueryParam("v", videoURL);
+      $("#thumbnails").empty();
+      if(theID != null) {
+      $('<div></div>')
+      .html(
+      '<h2>Click on a below image and Right Click on it at next Tab and Click "Save Image As..." to Download the image</h2><h3>&#187 Default Thumbnail</h3><a href="http://img.youtube.com/vi/' + theID + '/default.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/default.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a><h3>&#187 3 Other Small Thumbnails</h3><a href="http://img.youtube.com/vi/' + theID + '/1.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/1.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a><a href="http://img.youtube.com/vi/' + theID + '/2.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/2.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a><a href="http://img.youtube.com/vi/' + theID + '/3.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/3.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a><h3>&#187 Medium Quality Thumbnails</h3><a href="http://img.youtube.com/vi/' + theID + '/mqdefault.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/mqdefault.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a><br/><a href="http://img.youtube.com/vi/' + theID + '/0.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/0.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a><h3>&#187 High Quality Thumbnails</h3><a href="http://img.youtube.com/vi/' + theID + '/hqdefault.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/hqdefault.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a><h3>&#187 High Definition Thumbnail ( **Only available if, your video is more than 720p )</h3><a href="http://img.youtube.com/vi/' + theID + '/maxresdefault.jpg" target="_blank"><img src="http://img.youtube.com/vi/' + theID + '/maxresdefault.jpg"><figcaption>Download by: Right Click and Save Image as</figcaption></a>').appendTo("#thumbnails");
+      } else {
+      $('<div></div>').html('<div id="sorry">It seems like your "url" is wrong please check and retry</div>').appendTo("#thumbnails");
+      }
+      });
+    
+    });
+      function showHide() {
+      var div = document.getElementById("hidden_div");
+      if (div.style.display == 'none') {
+      div.style.display = '';
+      }
+      else {
+      div.style.display = '';
+      }
+    }
+</script>
+</body>
+
+</html>
+
+</center>
+
+  </div>
+</div>
+  
+</div>
+</div>   
+<h2>About Alexa Rank Checker</h2><br>
+<b>
+Alexa site rank furnished by searchenginereports.com will arrive handy if want to examine the standing of your website on a regular basis. All You should do is to write down the website URL in Area provided and click on over the “Examine” button.
+
+This Resource generates a traffic record graph for any chosen website page. It computes the website traffic of a particular web site by evaluating the world wide web utilization of Alexa Toolbars people, which happen to be anticipated to generally be millions of folks. With the knowledge it provides, you are able to Examine if the Website positioning procedures you've utilised to spice up your site’s popularity are successful or not.
+
+Alexa ranking is a straightforward ranking procedure for almost any Site which happens to be an presenting of alexa.com. By using a straightforward algorithm, alexa.com measures the frequency of views on a web site. The targeted traffic is calculated dependant on parameters like reach and webpage views of any webpage.
+
+A relative standard of viewers overlap between this site and related sites. Viewers overlap rating is calculated from an analysis of prevalent visitors and/or research search phrases.
+
+Alexa has made over time to include a paid out selection that features a suite of tools for examining Search engine optimization, conducting key phrase audits, and examining your competitor's effectiveness.
+
+To totally utilize this, the authority with the website page is ranked concerning the figures one-a hundred. The implication of this is that the page will only get a higher ranking if it's the chance to be ranked significant.
+
+Examine how well your web page is optimized to the concentrate on key phrase. We’ll operate a report and offer you actionable ways to increase your probability of ranking.
+
+Alexa can be a subsidiary business of Amazon.com that was acquired during the 12 months 1999. This is a California dependent organization that specializes in delivering web website traffic information and facts collected from different resources like World wide web browser extensions and toolbars.
+
+A relative degree of viewers overlap between This page and related web-sites. Audience overlap score is calculated from an Assessment of common guests and/or lookup key terms.
+
+And, Alivenet Remedy thinks that it's important for every business enterprise and entrepreneurs to evaluation their Site rankings periodically and make sure it progresses in the direction of the bottom quantity. The Alexa Visitors Rank Checker is quick and simple to use. It helps in keeping track of crucial parameters to raise site visitors and income and generate a lot more revenue.
+
+And the website Using the least variety is going to be ranked around 40 million. Additionally, if Alexa’s Site rank checker can not get to a specific Internet site for over the past a few months, there'll be no rank at all for the website.
+
+Smart shows Take care of your calendar, abide by as well as recipes, check here catch up on news plus more with Alexa.
+
+Many of our prospects are very well-known organizations. They opt for us thanks to our know-how and discretion. Should they trust us to boost their Site rank, why shouldn’t you?
+
+Users usually get drawn to one thing distinctive and so they finally become the automatic promoters for the web site which only Advantages it. So it only ensures that-
+
+
+</b>
+
+  	</div>
+</div> <br /> <br />
+
+
+<?php include'footer.php'; ?>  
+  
+	
+</body>
+</html>
+
